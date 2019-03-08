ebf9f62206fc3725c26e3aa165ce79ca96ddf79c
schktjm <si2.o.mjt@gmail.com>
2019-03-08 17:40:22 +0900

    feature/list

--- src/components/List.vue
+++ src/components/List.vue
@@ -1,5 +1,28 @@
-<template></template>
+<template>
+    <draggable v-model="list" group="list">
+        <list-item v-for="(item,idx) in list" :key="idx" :item="item"></list-item>
+    </draggable>
+</template>
 
-<script></script>
+<script>
+    import draggable from 'vuedraggable';
+    import ListItem from './ListItem';
+
+    export default {
+        name: 'List',
+        components: {ListItem, draggable},
+        computed: {
+            list: {
+                get() {
+                    return this.$store.state.list;
+                },
+                set(newlist) {
+                    this.$store.commit('updateList', {list: newlist})
+                }
+
+            }
+        }
+    }
+</script>
 
 <style lang="scss" scoped></style>
