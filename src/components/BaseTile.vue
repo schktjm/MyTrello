04951665559d9aa9e19ad21f433b7e9695ba0df4
schktjm <si2.o.mjt@gmail.com>
2019-03-17 17:49:33 +0900

    hum

---
+++ src/components/BaseTile.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="tile">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'BaseTile'
+    }
+</script>
+
+<style lang="scss" scoped></style>
--- src/components/List.vue
+++ src/components/List.vue
@@ -1,16 +1,19 @@
 <template>
-    <draggable v-model="list" group="list">
-        <list-item v-for="(item,idx) in list" :key="idx" :item="item"></list-item>
-    </draggable>
+    <base-tile class="is-parent is-4">
+        <draggable v-model="list" group="list">
+            <list-item v-for="(item,idx) in list" :key="idx" :item="item"></list-item>
+        </draggable>
+    </base-tile>
 </template>
 
 <script>
+    import BaseTile from './BaseTile';
     import draggable from 'vuedraggable';
     import ListItem from './ListItem';
 
     export default {
         name: 'List',
-        components: {ListItem, draggable},
+        components: {BaseTile, ListItem, draggable},
         computed: {
             list: {
                 get() {
