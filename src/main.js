306330d8ca98bc0eb4254f76501f86b149e5679e
schktjm <si2.o.mjt@gmail.com>
2019-03-18 14:51:18 +0900

    feature/add-new-item

--- src/main.js
+++ src/main.js
@@ -3,8 +3,19 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import Vuex from 'vuex'
+import {library} from '@fortawesome/fontawesome-svg-core'
+import {faCoffee, faPlusSquare, faPlus, faBan} from '@fortawesome/free-solid-svg-icons'
+import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome'
 
-Vue.config.productionTip = false
+library.add(faCoffee);
+library.add(faPlusSquare);
+library.add(faPlus);
+library.add(faBan);
+
+Vue.component('font-awesome-icon', FontAwesomeIcon);
+
+
+Vue.config.productionTip = false;
 
 Vue.use(Vuex);
 
